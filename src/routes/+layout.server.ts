f9788570ff3320b52882e3a2511acd4870bbbdba
cherrybll17253 <zeekee52@gmail.com>
2023-12-19 00:12:48 +0900

    Fixed bug on loging in on vercel

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,3 +1,8 @@
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+ 
+inject({ mode: dev ? 'development' : 'production' });
+
 import { env } from '$env/dynamic/private';
 import 'dotenv/config';
 import type { LayoutServerLoad } from './$types';
